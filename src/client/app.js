ae8a6cdbc2f194c71e41f097349b1f4098ecb49a
Tom Hastjarjanto <tom@intellicode.nl>
2016-04-12 20:50:38 +0200

    Integrate redux

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
 import X from './x';
+
+function reducer(state = 0, action) {
+  return state;
+}
+
+let store = createStore(reducer);
+
 ReactDOM.render(
   <div>
     <X />
