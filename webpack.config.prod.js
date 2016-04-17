920a2ed71ad1e5148446289a84ce48ca80b47fc8
Tom Hastjarjanto <tom@intellicode.nl>
2016-04-17 23:09:51 +0200

    Add production config for postcss

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,38 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
-    entry: [
-      './src/client/app.js'
+  entry: [
+    './src/client/app.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'js/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: path.join(__dirname, 'src/client'),
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader'),
+      },
     ],
-    output: {
-        path: __dirname,
-        filename: 'build/js/bundle.js',
-    },
-    module: {
-      loaders: [
-        { test: /\.jsx?$/, loaders: ['babel?cacheDirectory'], include: path.join(__dirname, 'src/client') },
-      ]
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('production')
-        }
-      })
-    ],
+  },
+  postcss() {
+    return [require('autoprefixer'), require('precss')];
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new ExtractTextPlugin('app.css'),
+  ],
 };
