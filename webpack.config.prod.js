e2044a5b6cc5191691c08f13c42af43ffdf2745a
Tom Hastjarjanto <tom@intellicode.nl>
2016-04-17 20:13:22 +0200

    Add production webpack config

---
+++ webpack.config.prod.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+    entry: [
+      './src/client/app.js'
+    ],
+    output: {
+        path: __dirname,
+        filename: 'build/assets/bundle.js',
+    },
+    module: {
+      loaders: [
+        { test: /\.jsx?$/, loaders: ['babel?cacheDirectory'], include: path.join(__dirname, 'src/client') },
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      })
+    ],
+};
